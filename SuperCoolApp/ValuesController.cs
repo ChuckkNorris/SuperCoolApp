277cef133e3e73a5568c405bf891ccc336eb7fc4
Levi Fuller <lfuller941@gmail.com>
2017-08-06 22:02:43 -0600

    To CI and beyond

--- SuperCoolApp/ValuesController.cs
+++ SuperCoolApp/ValuesController.cs
@@ -15,7 +15,7 @@ namespace SuperCoolApp
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Bro", "Do", "You", "Even", "Code?", "CI" };
+            return new string[] { "Continuous", "Integration", "is", "Pretty", "Neat!" };
         }
 
     }
