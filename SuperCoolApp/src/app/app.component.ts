1241236fd5b6fd2bc3d0dbb260256e31162706e7
Levi Fuller <lfuller941@gmail.com>
2017-08-06 23:15:33 -0600

    this code be chill

--- SuperCoolApp/src/app/app.component.ts
+++ SuperCoolApp/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Http } from '@angular/http'
 export class AppComponent implements OnInit {
 
     constructor(private _httpService: Http) { }
-
+    title: string = "This code be chill";
     apiValues: string[] = [];
     ngOnInit() {
         this._httpService.get('/api/values').subscribe(values => {
