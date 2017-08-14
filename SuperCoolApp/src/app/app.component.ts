5b0479f352de6485530d0fcdbc03ed72a6a11ddf
Levi Fuller <lfuller941@gmail.com>
2017-08-13 22:55:25 -0600

    test

--- SuperCoolApp/src/app/app.component.ts
+++ SuperCoolApp/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Http } from '@angular/http'
 export class AppComponent implements OnInit {
 
     constructor(private _httpService: Http) { }
-    title: string = "The ultimate test v2!";
+    title: string = "It is ready...";
     apiValues: string[] = [];
     ngOnInit() {
         this._httpService.get('/api/values').subscribe(values => {
